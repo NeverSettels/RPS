87338e41ebaf989b6616b0214205b205f4ada59b
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-13 15:04:20 -0700

    Functioning console rock paper scissors game where the user faces the cpu

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 using System;
-
+using RPS.Models;
 namespace RPS
 {
   public class Program
   {
+    
+    public static void TypeSlow(string str)
+    {
+      for (int i = 0; i < str.Length; i++)
+      {
+        Console.Write(str[i]);
+        System.Threading.Thread.Sleep(50);
+      }
+      Console.WriteLine();
+    }
     public static void Main()
     {
-      // Place used Methods here:
-      // Play();
-      // Rock();
-      // Paper();
-      // Scissors();
+      TypeSlow("Let's play a game of Rock, Paper, Scissors!");
+      TypeSlow("Enter your choice: ");
+      string choice = Console.ReadLine();
+      string adversary = CPUMakeChoice();
+      string winner = GetWinner(choice, adversary);
+      if(winner=="draw")
+      {
+        TypeSlow("You Tied");
+      }
+      else
+      {
+        TypeSlow($"You played: {choice}");
+        TypeSlow(".........");
+        TypeSlow($"CPU played {adversary}");
+        TypeSlow(".........");
+        TypeSlow($"{winner} wins!");
+      }
     }
   }
 }
--- RPS/Models/Rock.cs
+++ RPS/Models/Rock.cs
@@ -1,13 +1,75 @@
+using System;
+using System.Collections.Generic;
 
-
-namespace RPS
+namespace RPS.Models
 {
   public class RockPaperScissors
   {
     public static string GetWinner(string choice, string CPUchoice)
     {
-     return "hello";
+      string winner = "";
+      if(choice==CPUchoice)
+      {
+        winner = "draw";
+      }
+      else if((choice == "rock" && CPUchoice == "scissors") || (choice == "scissors" && CPUchoice == "paper") || (choice == "paper" && CPUchoice == "rock"))
+      {
+        winner = "player";
+      }
+      else 
+      {
+        winner = "cpu";
+      }
+     return winner;
+    }
+
+    public static string CPUMakeChoice()
+    {
+      List<string> options = new List<string>{"rock","paper","scissors"};
+      Random random = new Random();
+      int rIndex = random.Next(0, 4);
+      string choosen = options[rIndex];
+      return choosen;
+    }
+
+
+
+
+
+
+     public static void TypeSlow(string str)
+    {
+      for (int i = 0; i < str.Length; i++)
+      {
+        Console.Write(str[i]);
+        System.Threading.Thread.Sleep(50);
+      }
+      Console.WriteLine();
+    }
+    public static void Main()
+    {
+      TypeSlow("Let's play a game of Rock, Paper, Scissors!");
+      TypeSlow("Enter your choice: ");
+      string choice = Console.ReadLine();
+      string adversary = CPUMakeChoice();
+      string winner = GetWinner(choice, adversary);
+      TypeSlow($"You played: {choice}");
+      TypeSlow(".........");
+      TypeSlow($"CPU played {adversary}");
+      
+      if(winner=="draw")
+      {
+        TypeSlow(".........");
+        TypeSlow("You Tied");
+      }
+      else
+      {
+      
+        TypeSlow(".........");
+        TypeSlow($"{winner} wins!");
+      }
     }
   }
+  
 
 }
